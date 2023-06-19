0a5416ba6c9108683b6bfc7476cab92f83dde336
Luís Clício <cliciosa14@gmail.com>
2023-06-19 09:18:31 -0300

    feat: connect API to MongoDB

--- services/api/src/index.js
+++ services/api/src/index.js
@@ -1,127 +1,63 @@
-const { chromium } = require('playwright');
-const express = require('express');
-const { Pool } = require('pg');
-const path = require('path');
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = 3000;
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
 
 const app = express();
-require('dotenv').config();
-const PORT = process.env.PORT || 3333;
+const mongoClient = new MongoClient(MONGO_URL);
 
 app.use(express.json());
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+
+async function getCollection(name) {
+  await mongoClient.connect();
+  const database = mongoClient.db('links-saver');
+  const collection = database.collection(name);
+  return collection;
+}
+
+app.post('/links', async (req, res) => {
+  const { url } = req.body;
+
+  try {
+    // insert link to rabbitmq
+
+    return res.status(201).send({ success: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ success: false });
+  }
 });
 
-async function checkURLTable() {
+app.get('/links', async (req, res) => {
   try {
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS url (
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(255),
-        description TEXT,
-        img VARCHAR(255),
-        url TEXT
-      );
-    `;
-    await pool.query(createTableQuery);
-    console.log("Tabela de URL verificada ou criada com sucesso");
-  } catch (error) {
-    console.error("Erro ao verificar ou criar tabela de URL:", error);
-  }
-}
+    const collection = await getCollection('links');
+    const links = await collection.find().toArray();
 
-app.post('/addURL', async (req, res) => {
-  const urlA = req.body.url;
-
-  const pageMetadata = await getPageMetadata({
-    url: urlA,
-    headless: true,
-  });
-
-
-  console.log(pageMetadata);
-
-  try{
-      const { newUbs } = await pool.query('INSERT INTO url (title, description, img, url) VALUES ($1, $2, $3, $4)', [pageMetadata.title, pageMetadata.description, pageMetadata.screenshotPath, urlA])
-      return res.status(201).send("URL cadastrada com sucesso")
-  } catch(erro) {
-      res.status(500).send(erro);
-  }
-})
-
-app.get('/allURL', async (req, res) => {
-  try{
-    const { rows } = await pool.query(`SELECT * FROM url`)
-    return res.status(200).send(rows)
-  } catch(erro) {
-      return res.status(400).send(erro)
-  }
-})
-
-
-async function getBrowser({ headless = false }) {
-  try {
-    const browser = await chromium.launch({ headless, slowMo: 50 });
-    return browser;
+    return res.status(200).send({
+      success: true,
+      links,
+    });
   } catch (error) {
     console.error(error);
-    return null;
-  }
-}
-
-async function getPageMetadata({ url, headless = false }) {
-  const browser = await getBrowser({ headless });
-
-  if (!browser) {
-    return null;
-  }
-
-  try {
-    const page = await browser.newPage();
-    await page.goto(url);
-    const title = await page.title();
-    const description = await getDescription({ page });
-    const screenshotPath = await saveScreenshot({ page });
-
-    return {
-      title,
-      description,
-      screenshotPath,
-    };
-  } catch (error) {
-    console.error(error);
-    return null;
+    return res.status(500).send({
+      success: false,
+      links: [],
+    });
   } finally {
-    await browser.close();
+    await mongoClient.close();
   }
-}
+});
 
-async function getDescription({ page }) {
-  try {
-    const description = await page.$eval(
-      'meta[name="description"]',
-      (element) => element.getAttribute('content')
-    );
+app.listen(PORT, () => {
+  console.log(`=> Server listening on port ${PORT}...`);
+});
 
-    return description;
-  } catch (error) {
-    console.error(error);
-    return '';
-  }
-}
-
-async function saveScreenshot({ page, folder = 'screenshots' }) {
-  try {
-    const randomID = Math.random().toString(36).substring(7); // Trocar por UUID ao invés de random
-    const filePath = path.join(folder, `${randomID}.png`);
-    await page.screenshot({ path: filePath });
-    return filePath;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
-
-checkURLTable().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+process.on('uncaughtException', async (error) => {
+  console.error(error);
+  await mongoClient.close();
+  process.exit(1);
 });
